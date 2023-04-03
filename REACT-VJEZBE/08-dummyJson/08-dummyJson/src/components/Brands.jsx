c8864f69e4d9fbb654a20ac8197a998b881567dc
Edita Krizmanic <ekrizmanic01@gmail.com>
2023-04-04 00:12:29 +0200

    class component added

--- REACT-VJEZBE/08-dummyJson/08-dummyJson/src/components/Brands.jsx
+++ REACT-VJEZBE/08-dummyJson/08-dummyJson/src/components/Brands.jsx
@@ -1,19 +1,23 @@
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Brands = ({ data, activePage }) => {
-  const uniqueData = Array.from(new Set(data.map((item) => item.brand)))
+class Brands extends React.Component {
+  render() {
+    const { data, activePage } = this.props;
+    const uniqueData = Array.from(new Set(data.map((item) => item.brand)));
 
-  return (
-    <>
-      {activePage.includes("brands") &&
-        uniqueData.map((brand) => (
-          <div key={brand}>
-            <h2>{brand}</h2>
-          </div>
-        ))}
-    </>
-  );
-};
+    return (
+      <>
+        {activePage.includes('brands') &&
+          uniqueData.map((brand) => (
+            <div key={brand}>
+              <h2>{brand}</h2>
+            </div>
+          ))}
+      </>
+    );
+  }
+}
 
 Brands.propTypes = {
   brand: PropTypes.string
